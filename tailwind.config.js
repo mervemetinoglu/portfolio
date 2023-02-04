9737659aa74ef9549679a122231f9817eb4576ae
Merve Metinoglu <mervemetinogluu@gmail.com>
2023-02-04 21:54:42 +0300

    style: added colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
@@ -10,17 +11,24 @@ module.exports = {
 	theme: {
 		colors: {
 			transparent: 'transparent',
-			night: 'rgba(21, 21, 21, 1)',
+			night100: '#121111',
 			white: '#ffffff',
-			orange400: '#fb923c',
-			emerald600: '#059669',
-			teal700: '#0f766e',
-			teal800: '#115e59',
+			amber50: '#fffbeb',
+			amber100: '#fef3c7',
 			gray400: '#9ca3af',
+			gray600: '#4b5563',
+			gray800: '#1f2937',
+			zinc700: '#3f3f46',
+			zinc50: '#fafafa',
 			zinc400: '#a1a1aa',
+			zinc800: '#27272a',
+			zinc900: '#18181b',
+			stone700: '#44403c',
 			neutral400: '#a3a3a3',
 			neutral500: '#737373',
-			gray100: 'rgba(148, 148, 149, 1)',
+			teal700: '#0f766e',
+			teal800: '#115e59',
+			cyan800: '#155e75',
 		},
 		extend: {
 			fontFamily: {
@@ -28,8 +36,7 @@ module.exports = {
 			},
 		},
 		variants: {
-			width: ['responsive', 'hover', 'focus'],
+			width: ['responsive', 'hover'],
 		},
 	},
-	plugins: [],
 };
