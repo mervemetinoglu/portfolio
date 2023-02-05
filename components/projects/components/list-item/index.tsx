8eca8a11e8e9f2bb66e3cfeec55fa28b572b1c71
Merve Metinoglu <mervemetinogluu@gmail.com>
2023-02-06 00:08:43 +0300

    updated animation

--- components/projects/components/list-item/index.tsx
+++ components/projects/components/list-item/index.tsx
@@ -9,25 +9,23 @@ export const ProjectsListItem = ({ projectItem }: IProjectsListItemProps) => {
 	const toolsWithComma = projectItem.tools.map((item) => item.name).join(', ');
 
 	return (
-		<li className="group relative mb-6 flex h-32 cursor-pointer items-center px-2 md:h-40">
+		<li className="group relative mb-6 flex h-32 cursor-pointer items-center overflow-hidden px-2 md:h-40">
 			<Link
 				target="_blank"
 				href={projectItem.url}
-				className="absolute top-0 left-0 bottom-0 right-0 w-full bg-transparent"
 				aria-label={projectItem.name}
+				className="absolute top-0 left-0 bottom-0 right-0 w-full bg-transparent"
 			/>
-			<div className="ml-5 hidden w-80 duration-1000 group-hover:block">
-				<span className="text-xs text-zinc400 md:text-justify md:text-base">
+			<div className="absolute -left-1/2 w-1/2 duration-1000 group-hover:left-0">
+				<span className="inline-block text-justify text-xs text-zinc400 md:text-base">
 					{projectItem.description}
 				</span>
 			</div>
-			<div className="text-md w-56 font-bold text-zinc50 duration-1000 group-hover:translate-x-28 md:w-96 md:text-2xl">
+			<div className="text-md absolute left-0 w-1/2 px-7 font-bold text-zinc50 duration-1000 group-hover:left-1/2 group-hover:text-right md:text-2xl">
 				{projectItem.name}
 			</div>
-			<div className="ml-5 w-full group-hover:hidden">
-				<span className="text-xs text-zinc400 md:text-justify md:text-base">
-					{toolsWithComma}
-				</span>
+			<div className="absolute left-1/2 w-1/2 text-xs text-zinc400 duration-1000 group-hover:left-full  md:text-base">
+				<span>{toolsWithComma}</span>
 			</div>
 		</li>
 	);
